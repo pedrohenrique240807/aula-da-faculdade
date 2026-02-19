10c01bb41cd4f6965708dc2baf35c207107ec455
Your Name <you@example.com>
2026-02-19 08:20:45 -0300

    primeiro

---
+++ python.py
@@ -0,0 +1,15 @@
+nome=input('qual seu nome?')
+print('ola', nome, 'seja bem vindo ao meu mundo')
+
+email= input('digite seu email!')
+print(email)
+
+senha= input('crie uma senha')
+print('sua senha é:' ,senha)
+
+nome_de_usuario = input('digite um nome de usuario')
+print(nome_de_usuario)
+
+
+
+
