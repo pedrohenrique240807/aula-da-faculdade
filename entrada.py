7dab82cad8061dcc81392d3130139639fa6fe18f
Your Name <you@example.com>
2026-02-19 07:49:55 -0300

    aula

---
+++ entrada.py
@@ -0,0 +1,13 @@
+nome=input('qual seu nome?')
+print ('ola', nome,' ,seja bem vindo ao meu mundo')
+
+email=input('qual é seu email?')
+print(email)
+
+senha= input('digite uma senha')
+print (senha)
+
+nome_de_usuario=input('digite um nome de usuario')
+print(nome_de_usuario)
+
+print(nome+'@'+'unianchieta.gmail.com')
